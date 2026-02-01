8921274b8e8eb92282005b6cd09f32d2fc87dc89
b1342 <batya90425@gmail.com>
2026-02-01 20:04:26 +0200

    Upgrade Angular 9 -> 10 (Node 14 compatible)

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -22,6 +22,8 @@
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 import 'classlist.js';  // Run `npm install --save classlist.js`.
 
+ 
+
 /** Evergreen browsers require these. **/
 //import 'core-js/es6/reflect';
 
